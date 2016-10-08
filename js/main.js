ec9501e7456fdb0b0aae29813b16d0f81cdcf6e0
Michał Cichoń <michal.cichon@codete.com>
2016-10-08 16:20:54 +0200

    new look init

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function foldMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
