f05d543feb293adad8604345f212e7bca5bdfd48
Michał Cichoń <michal.cichon@codete.com>
2017-03-10 09:48:25 +0100

    Some small alignments

--- js/main.js
+++ js/main.js
@@ -7,5 +7,22 @@ function foldMenu() {
     }
 }
 
+function init() {
+    setYearInFooter();
+}
+
+function setYearInFooter () {
+    var currentTime = new Date();
+
+    document.getElementById('currentYear').innerHTML = currentTime.getFullYear();
+}
+
+var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+        clearInterval(readyStateCheckInterval);
+        init();
+    }
+}, 10);
+
 
 console.log("██╗  ██╗███████╗██╗     ██╗      ██████╗ \n██║  ██║██╔════╝██║     ██║     ██╔═══██╗\n███████║█████╗  ██║     ██║     ██║   ██║\n██╔══██║██╔══╝  ██║     ██║     ██║   ██║\n██║  ██║███████╗███████╗███████╗╚██████╔╝\n╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝ ╚═════╝ \n\n    ████████╗██╗  ██╗███████╗██████╗ ███████╗██╗\n    ╚══██╔══╝██║  ██║██╔════╝██╔══██╗██╔════╝██║\n       ██║   ███████║█████╗  ██████╔╝█████╗  ██║\n       ██║   ██╔══██║██╔══╝  ██╔══██╗██╔══╝  ╚═╝\n       ██║   ██║  ██║███████╗██║  ██║███████╗██╗\n       ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚══════╝╚═╝\n\nThere is nothing special here but good to see you anyways!");
