a050a49144929bad604e9439f0227108e0cfa0a3
Michał Cichoń <michal.cichon@codete.com>
2021-11-06 12:24:37 +0100

    Darkmode in a separate css file

--- js/main.js
+++ js/main.js
@@ -21,11 +21,52 @@ function closeMenuIfOpened() {
 
 function init() {
     setYearInFooter();
+    refreshLightModeButton();
 }
 
-function setYearInFooter () {
+function setYearInFooter() {
     var currentTime = new Date();
     document.getElementById('currentYear').innerHTML = currentTime.getFullYear();
+}
+
+function toggleDarkmode() {
+    var c = getCookie("darkmode")
+    if (c == 1) {
+        document.cookie = `darkmode=0`
+    } else {
+        document.cookie = `darkmode=1`
+    }
+    window.location.reload(true);
+}
+
+function getCookie(cName) {
+    const name = cName + "=";
+    const cDecoded = decodeURIComponent(document.cookie); //to be careful
+    const cArr = cDecoded .split('; ');
+    let res;
+    cArr.forEach(val => {
+        if (val.indexOf(name) === 0) res = val.substring(name.length);
+    })
+    return res;
+}
+
+function addDarkmodeCSS() {
+    var timestamp = new Date().getTime();
+    var element = document.createElement("link");
+    element.setAttribute("rel", "stylesheet");
+    element.setAttribute("type", "text/css");
+    element.setAttribute("href", "/css/darkmode.min.css?t="+timestamp);
+    document.getElementsByTagName("head")[0].appendChild(element);
+}
+
+function refreshLightModeButton() {
+    var c = getCookie("darkmode")
+    if (c == 1) {
+        document.getElementById('light-mode-button').innerHTML = "☀ Light mode";
+        addDarkmodeCSS();
+    } else {
+        document.getElementById('light-mode-button').innerHTML = "☀ Dark mode";
+    }
 }
 
 var readyStateCheckInterval = setInterval(function() {
--- reduce.sh
+++ reduce.sh
@@ -1,5 +1,6 @@
 echo 'Minifying CSS...';
 reduce css/main.css > css/main.min.css
+reduce css/darkmode.css > css/darkmode.min.css
 
 echo 'Minifying JS...';
 reduce js/main.js > js/main.min.js
